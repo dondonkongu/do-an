853294bdde3aea5d93cd985e3c5014a05cb5dacf
dondonkongu <100260146+dondonkongu@users.noreply.github.com>
2024-10-03 11:12:00 +0700

    inittial commit

---
+++ identity-service/src/test/java/com/thanhdon/identity_service/IdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanhdon.identity_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
