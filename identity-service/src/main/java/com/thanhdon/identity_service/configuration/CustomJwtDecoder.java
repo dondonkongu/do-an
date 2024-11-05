bbe2771959ac01673f937529b9a04247d85cfebf
dondonkongu <100260146+dondonkongu@users.noreply.github.com>
2024-11-05 22:59:27 +0700

    authorization-between-apigateway-identity

--- identity-service/src/main/java/com/thanhdon/identity_service/configuration/CustomJwtDecoder.java
+++ identity-service/src/main/java/com/thanhdon/identity_service/configuration/CustomJwtDecoder.java
@@ -3,40 +3,27 @@ package com.thanhdon.identity_service.configuration;
 import java.text.ParseException;
 
 import com.nimbusds.jwt.SignedJWT;
-import com.thanhdon.identity_service.service.AuthenticationService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtException;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomJwtDecoder implements JwtDecoder {
-    @Value("${jwt.signerKey}")
-    private String signerKey;
-
-    private final AuthenticationService authenticationService;
-
-    private NimbusJwtDecoder nimbusJwtDecoder = null;
-
-    public CustomJwtDecoder(AuthenticationService authenticationService) {
-        this.authenticationService = authenticationService;
-    }
-
     @Override
     public Jwt decode(String token) throws JwtException {
-
         try {
             SignedJWT signedJWT = SignedJWT.parse(token);
-            return new Jwt(token,signedJWT.getJWTClaimsSet().getIssueTime().toInstant(),
+
+            return new Jwt(token,
+                    signedJWT.getJWTClaimsSet().getIssueTime().toInstant(),
                     signedJWT.getJWTClaimsSet().getExpirationTime().toInstant(),
                     signedJWT.getHeader().toJSONObject(),
-                    signedJWT.getJWTClaimsSet().getClaims());
+                    signedJWT.getJWTClaimsSet().getClaims()
+            );
 
         } catch (ParseException e) {
-            throw new JwtException("invalid token");
+            throw new JwtException("Invalid token");
         }
-
     }
 }
